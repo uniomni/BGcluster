cb32dcf11918b2a35349918151a1c85d29e7717a
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2013-07-03 12:32:33 +0700

    With default command

--- src/exec_remote_sudo.py
+++ src/exec_remote_sudo.py
@@ -14,7 +14,7 @@ python exec_cluster.py python setup.py install
 """
 
 # Sources:
-#http://stackoverflow.com/questions/15166973/sending-a-password-over-ssh-or-scp-with-subprocess-popen
+# http://stackoverflow.com/questions/15166973/sending-a-password-over-ssh-or-scp-with-subprocess-popen
 # http://www.unix.com/programming/176212-using-commands-over-ssh-using-sudo.html
 
 import os
@@ -23,7 +23,9 @@ import getpass
 import subprocess
 
 
-def run_remote(username, host, password, directory=None, command='whoami'):
+def run_remote(username, host, password, directory=None, 
+               command=None,
+               verbose=True, debug=True):
     """Run command on remote host as sudo
 
     Args
@@ -31,20 +33,39 @@ def run_remote(username, host, password, directory=None, command='whoami'):
     username: user id under which to run. Must have sudo access.
     host: hostname or ip address of remote host
     directory: directory in which to run the command. If None cd to ~
-    command: unix command to run. Default to 'whoami' (for testing). 
+    command: unix command to run. Defaults to 'whoami' (for testing). 
     """
-    logfile = '/var/tmp/last_remote_exec_%s.log' % (command.replace(' ', '_'))
+
+    if command is None:
+        # Default command for diagnostic purposes
+        #command = 'echo -n `whoami`; echo -n @; echo `hostname`'
+        command = 'echo `whoami`'
 
     if directory is None:
         directory = '~'
 
+    command_sanitized = command.replace(' ', '_').replace('`','').replace(';', ':')
+    logfile = '/var/tmp/last_remote_exec_%s.log' % command_sanitized
+
+    if verbose:
+        print 'Running command "%s" on %s in directory %s. See logfile %s for details.' % (command, host, cwd, logfile)
     p = subprocess.Popen(['ssh', '-t', '-t', '%s@%s' % (username, host),
                           'cd %s; sudo %s > %s' % (directory, command, logfile)],
-                         stdin=subprocess.PIPE)
+                         stdin=subprocess.PIPE, 
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+
 
     # Write password to process.
     # Use variable either from first time input or file
-    p.stdin.write('%s\n' % password) 
+    p.stdin.write('%s\n' % password)
+    if debug:
+        print 'stdout: "%s"' % p.stdout.read()
+        print 'stderr: "%s"' % p.stderr.read()
+    
+    if verbose:
+        print
+
 
 def usage():
     s = 'Usage:\t'
@@ -54,21 +75,23 @@ def usage():
 
 if __name__ == '__main__':
     
-    if len(sys.argv) == 1:
-        print usage()
-        sys.exit() 
-    else:
+    if len(sys.argv) > 1:
         command = ' '.join(sys.argv[1:])
+    else:
+        command = None
 
     # Get sudo password (must be the same on all hosts)
-    username = raw_input('Please enter sudo username (must be the same for all nodes): ')
-    password = getpass.getpass('Please enter sudo password (must be the same for all nodes): ')
+    #username = raw_input('Please enter sudo username (must be the same for all nodes): ')
+    #password = getpass.getpass('Please enter sudo password (must be the same for all nodes): ')
+    username = 'uniomni'
+    password = 'dstat43'
 
     # Get current working directory
     cwd = os.getcwd()
     
     # Run command remotely for each host
-    for host in ['tambora', 'node1']:
-        print 'Running command "%s" on %s in directory %s. See logfile %s for details.' % (command, host, cwd, logfile)
+    #for host in ['tambora', 'node1', 'node2']:
+    for host in ['node1', 'node2']:
+        
 
-        run_remote(username, host, password, cwd, command)
+        run_remote(username, host, password, directory=cwd, command=None)
