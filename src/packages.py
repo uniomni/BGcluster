c6afa79eb16d73af302b6c227b5f124e0b8c5525
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2013-07-03 16:26:27 +0700

    Added Python-FALL3D dependencies

--- src/packages.py
+++ src/packages.py
@@ -3,5 +3,12 @@ debian_packages = [
     'openmpi-bin', 
     'libopenmpi-dev', 
     'python-dev', 
-    'python-numpy',]
+    'python-numpy',
+    # For Python-FALL3D
+    'python-numpy', 
+    'python-scientific',
+    'gfortran', 
+    'python-gdal', 
+    'gdal-bin', 
+    'libnetcdf-dev']
 
